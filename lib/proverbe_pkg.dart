c8cb62cc1af4bb2d5e9a321f57ada1d00d15dbf3
Emmanuel <eudeemmanuelassogba@gmail.com>
2022-06-23 13:38:18 +0200

    Made some changes

--- lib/proverbe_pkg.dart
+++ lib/proverbe_pkg.dart
@@ -1,7 +1,22 @@
 library proverbe_pkg;
+import 'dart:math';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class ListProverbs extends StatelessWidget {
+  final  style;
+
+  ListProverbs({Key? key,this.style}) : super(key: key);
+
+  List<String> proverbs = ["Il ne faut pas dire : fontaine, je ne boirai pas de ton eau.",
+      "A chacun son heure de gloire",
+    "A cheval donné on ne regarde pas la bride.",
+    "Il vaut mieux prévenir que guérir.",
+    "A cheval donné on ne regarde pas la bride. ",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(proverbs.elementAt(Random().nextInt(proverbs.length)));
+  }
 }
